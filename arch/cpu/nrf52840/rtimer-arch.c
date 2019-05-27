c07f5242e84c5fba731c20e1175e189b26ee51d6
George Oikonomou <g.oikonomou@bristol.ac.uk>
2019-05-27 23:08:37 +0100

    Re-implement rtimer bypassing nrf_drv/nrfx...
    * Frequency 62500: Exactly 16 usec / tick = exactly 1 .15.4 symbol period
    * Using Timer 0 in timer mode
      * Channel 0 in compare mode for rtimer interruts
      * Channel 1 used in capture mode so we can read out the current time

--- arch/cpu/nrf52840/rtimer-arch.c
+++ arch/cpu/nrf52840/rtimer-arch.c
@@ -1,81 +1,29 @@
-/*
- * Copyright (c) 2015 Nordic Semiconductor. All Rights Reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/**
- * \addtogroup nrf52832
- * @{
- *
- * \file
- *      Implementation of the architecture dependent rtimer functions for the nRF52
- *
- * \author
- *      Wojciech Bober <wojciech.bober@nordicsemi.no>
- */
 /*---------------------------------------------------------------------------*/
+#include "contiki.h"
+#include "nrf.h"
+#include "nrf_timer.h"
+
 #include <stdint.h>
 #include <stddef.h>
-#include "nrf.h"
-#include "nrf_drv_timer.h"
-#include "app_error.h"
-#include "contiki.h"
-
-static const nrf_drv_timer_t timer = NRF_DRV_TIMER_INSTANCE(PLATFORM_TIMER_INSTANCE_ID); /**< Timer instance used for rtimer */
-
-/**
- * \brief Handler for timer events.
- *
- * \param event_type type of an event that should be handled
- * \param p_context opaque data pointer passed from nrf_drv_timer_init()
- */
-static void
-timer_event_handler(nrf_timer_event_t event_type, void* p_context)
-{
-  switch (event_type) {
-    case NRF_TIMER_EVENT_COMPARE1:
-      rtimer_run_next();
-      break;
-
-    default:
-      //Do nothing.
-      break;
-  }
-}
 /*---------------------------------------------------------------------------*/
-/**
- * \brief Initialize platform rtimer
+/*
+ * Use Timer RTIMER_TIMER at 62500Hz. Generates 1 tick per exactly 16 usecs,
+ * which is exactly 1 .15.4 symbol period.
  */
+#define TIMER_INSTANCE NRF_TIMER0
+/*---------------------------------------------------------------------------*/
 void
 rtimer_arch_init(void)
 {
-  ret_code_t err_code = nrf_drv_timer_init(&timer, NULL, timer_event_handler);
-  APP_ERROR_CHECK(err_code);
-  nrf_drv_timer_enable(&timer);
+  nrf_timer_event_clear(TIMER_INSTANCE, NRF_TIMER_EVENT_COMPARE0);
+
+  nrf_timer_frequency_set(TIMER_INSTANCE, NRF_TIMER_FREQ_62500Hz);
+  nrf_timer_bit_width_set(TIMER_INSTANCE, NRF_TIMER_BIT_WIDTH_32);
+  nrf_timer_mode_set(TIMER_INSTANCE, NRF_TIMER_MODE_TIMER);
+  nrf_timer_int_enable(TIMER_INSTANCE, NRF_TIMER_INT_COMPARE0_MASK);
+  NVIC_ClearPendingIRQ(TIMER0_IRQn);
+  NVIC_EnableIRQ(TIMER0_IRQn);
+  nrf_timer_task_trigger(TIMER_INSTANCE, NRF_TIMER_TASK_START);
 }
 /*---------------------------------------------------------------------------*/
 /**
@@ -90,20 +38,21 @@ rtimer_arch_init(void)
 void
 rtimer_arch_schedule(rtimer_clock_t t)
 {
-  nrf_drv_timer_compare(&timer, NRF_TIMER_CC_CHANNEL1, t, true);
+  nrf_timer_cc_write(TIMER_INSTANCE, NRF_TIMER_CC_CHANNEL0, t);
 }
 /*---------------------------------------------------------------------------*/
-/**
- * \brief Returns the current real-time clock time
- * \return The current rtimer time in ticks
- *
- */
 rtimer_clock_t
 rtimer_arch_now()
 {
-  return nrf_drv_timer_capture(&timer, NRF_TIMER_CC_CHANNEL0);
+  nrf_timer_task_trigger(TIMER_INSTANCE, NRF_TIMER_TASK_CAPTURE1);
+  return nrf_timer_cc_read(TIMER_INSTANCE, NRF_TIMER_CC_CHANNEL1);
 }
 /*---------------------------------------------------------------------------*/
-/**
- * @}
- */
+void
+TIMER0_IRQHandler(void)
+{
+  if(nrf_timer_event_check(TIMER_INSTANCE, NRF_TIMER_EVENT_COMPARE0)) {
+    nrf_timer_event_clear(TIMER_INSTANCE, NRF_TIMER_EVENT_COMPARE0);
+    rtimer_run_next();
+  }
+}
