3cf5276303d44835f5c963ea4195ab82fe9e67aa
George Oikonomou <g.oikonomou@bristol.ac.uk>
2019-06-01 20:53:35 +0100

    Change debug level: invalid PHR (e.g. zero-len) in read() is not necessarily an error

--- arch/cpu/nrf52840/rf/nrf52840-ieee.c
+++ arch/cpu/nrf52840/rf/nrf52840-ieee.c
@@ -610,7 +610,7 @@ read_frame(void *buf, unsigned short bufsize)
   payload_len = rx_buf.phr - FCS_LEN;
 
   if(phr_is_valid(rx_buf.phr) == false) {
-    LOG_ERR("Incorrect length: %d\n", payload_len);
+    LOG_DBG("Incorrect length: %d\n", payload_len);
     rx_buf_clear();
     enter_rx();
     return 0;
