8a2ba199519885d13513dda65e7073a0a84e9ee3
Roget Kou <roget.kou@toshiba-trel.com>
2020-02-13 18:20:58 +0000

    writing init

--- arch/dev/dw1000/dw1000.c
+++ arch/dev/dw1000/dw1000.c
@@ -1,5 +1,8 @@
 #include "contiki.h"
+
 #include "deca_device_api.h"
+#include "port_platform.h"
+#include "mdek1001-def.h"
 
 #include "net/netstack.h"
 #include "net/packetbuf.h"
@@ -46,7 +49,39 @@ const struct radio_driver dw1000_driver =
  * DW1000 radio init function
  * 
  */
-static int init(void) {
-    /* Rest radio */
 
+/*DW1000 config function*/
+static dwt_config_t config = {
+    5,                /* Channel number. */
+    DWT_PRF_64M,      /* Pulse repetition frequency. */
+    DWT_PLEN_64,     /* Preamble length. Used in TX only. */
+    DWT_PAC8,         /* Preamble acquisition chunk size. Used in RX only. */
+    10,               /* TX preamble code. Used in TX only. */
+    10,               /* RX preamble code. Used in RX only. */
+    0,                /* 0 to use standard SFD, 1 to use non-standard SFD. */
+    DWT_BR_6M8,       /* Data rate. */
+    DWT_PHRMODE_STD,  /* PHY header mode. */
+    (65 + 8 - 8)     /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
+};
+
+
+static int init(void) {
+
+    int result;
+
+    /* Reset radio */
+    reset_DW1000();
+
+    /* Set 2MHz SPI for radio initialisation */
+    port_set_dw1000_slowrate();
+
+    /* Initialise the radio */
+    if (dwt_initialise(DWT_LOADUCODE) == DWT_ERROR) 
+        return -1;
+    
+    /* Set 8Mhz SPI for configuration and operation */
+    port_set_dw1000_fastrate();
+    dwt_configure(&config);
+
+    
 }
